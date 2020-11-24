d5636835fb6a60396df441b5c7898181855847ff
Salif Dianda <37021551+AutismPatient@users.noreply.github.com>
2020-11-24 18:00:12 +0800

    init

---
+++ src/components/tabbar/tabbar.js
@@ -0,0 +1,41 @@
+// components/tabbar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    selectd: 0,
+    color: "#7A7E83",
+    select_color: "#07c160",
+    list:[
+      {
+        path: "/pages/index",
+        icon: "",
+        selectIcon: "",
+        text: "首页",
+      },
+      {
+        path: "/pages/logs",
+        icon: "",
+        selectIcon: "",
+        text: "日志",
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    selectd(e){
+      const 
+    }
+  }
+})
---
+++ src/utils/user.js
@@ -0,0 +1,4 @@
+// 用户相关
+function userAuth(par){
+}
+export default userAuth
